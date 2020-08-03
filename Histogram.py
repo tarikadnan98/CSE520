7a6f07ce3b50a9fde407f510940626df320b2d80
tarikadnan98 <tarikadnan98@gmail.com>
2020-08-03 14:25:19 +0600

    Adding input method in Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+col_name = input("Type the feature name to Histogram: ")
 
-data = pd.read_csv('F:/File/Car_data.csv', quoting=3)['mpg']
+data = pd.read_csv('F:/File/Car_data.csv', quoting=3)[col_name]
 plt.hist(data, bins=20)
 plt.xlim([0, 40])
 plt.ylabel('Frequency')
 plt.xlabel('mpg')
-plt.title('Histogram of mpg')
+plt.title('Histogram of feature:' + " " + col_name)
 plt.show()
