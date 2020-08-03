94024a1af745f1c13c7286a44b215f94bd864a7a
tarikadnan98 <tarikadnan98@gmail.com>
2020-08-03 14:30:06 +0600

    Adding dynamic xlabel in Histogram.py

--- Histogram.py
+++ Histogram.py
@@ -6,6 +6,6 @@ data = pd.read_csv('F:/File/Car_data.csv', quoting=3)[col_name]
 plt.hist(data, bins=20)
 plt.xlim([0, 40])
 plt.ylabel('Frequency')
-plt.xlabel('mpg')
+plt.xlabel(col_name)
 plt.title('Histogram of feature:' + col_name)
 plt.show()
